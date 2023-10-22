c6791c2054dc4a68ddde90d0b1bde31a9e2b1a02
nouran <nouranesam189@gmail.com>
2023-10-22 16:50:32 -0700

    initial commit Quran ,Hadeth and settings without provider

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get islami => 'اسلامي';
+
+  @override
+  String get quran => 'قرأن';
+
+  @override
+  String get hadeth => 'حديث';
+
+  @override
+  String get sebha => 'تسبيح';
+
+  @override
+  String get radio => 'راديو';
+
+  @override
+  String get settings => 'الاعدادات';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get islami => 'Islami';
+
+  @override
+  String get quran => 'Quran';
+
+  @override
+  String get hadeth => 'Hadeth';
+
+  @override
+  String get sebha => 'Sebha';
+
+  @override
+  String get radio => 'Radio';
+
+  @override
+  String get settings => 'Settings';
+}
